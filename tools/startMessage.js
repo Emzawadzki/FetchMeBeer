72abab6dc1a06a01a699cf122a6f06b875b51f91
Michał Zawadzki <emzawadzki@gmail.com>
2018-08-09 14:28:54 +0200

    Init

---
+++ tools/startMessage.js
@@ -0,0 +1,5 @@
+import colors from 'colors';
+
+console.log('---------------------------'.green);
+console.log('*** Dev mode activated! ***'.green);
+console.log('---------------------------'.green);
